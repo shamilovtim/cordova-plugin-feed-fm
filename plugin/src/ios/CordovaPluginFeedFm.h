0c2892d42bdd793dc287415173636bf8306f55d8
Timothy Shamilov <tim@mothic.com>
2019-12-20 16:30:46 -0500

    init

---
+++ plugin/src/ios/CordovaPluginFeedFm.h
@@ -0,0 +1,11 @@
+/********* Echo.h Cordova Plugin Header *******/
+
+#import <Cordova/CDVPlugin.h>
+
+@interface CordovaPluginFeedFm : CDVPlugin
+
+- (void)echo:(CDVInvokedUrlCommand*)command;
+
+@end
+
+/********* Echo.m Cordova Plugin Implementation *******/
