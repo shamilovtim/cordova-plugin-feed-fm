01def0c8208816a09cf66f8c420cdbf12c962926
Timothy Shamilov <tim@mothic.com>
2019-12-24 21:38:38 -0500

    feat(android): implement simple echo

---
+++ plugin/src/android/CordovaPluginFeedFm.java
@@ -0,0 +1,32 @@
+package shamilovtim;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class CordovaPluginFeedFm extends CordovaPlugin {
+
+@Override
+public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("echo")) {
+        Log.i("CordovaPluginFeedFm", "Native echo call");
+        String message = args.getString(0);
+        this.echo(message, callbackContext);
+        return true;
+    }
+    return false;
+}
+
+private void echo(String message, CallbackContext callbackContext) {
+    if (message != null && message.length() > 0) {
+        callbackContext.success(message);
+    } else {
+        callbackContext.error("Expected one non-empty string argument.");
+    }
+}
+}
