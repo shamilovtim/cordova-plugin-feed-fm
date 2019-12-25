683a923f6d9f5141a1d6336d704019ba3c1c95a9
Timothy Shamilov <tim@mothic.com>
2019-12-25 00:53:16 -0500

    feat(android): implement play

--- plugin/src/android/CordovaPluginFeedFm.java
+++ plugin/src/android/CordovaPluginFeedFm.java
@@ -27,6 +27,11 @@ public class CordovaPluginFeedFm extends CordovaPlugin implements FeedAudioPlaye
             this.echo(message, callbackContext);
             return true;
         }
+        if (action.equals("play")) {
+            Log.i("CordovaPluginFeedFm", "Native play call");
+            this.play();
+            return true;
+        }
         return false;
     }
 
@@ -37,4 +42,82 @@ public class CordovaPluginFeedFm extends CordovaPlugin implements FeedAudioPlaye
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    public void play() {
+        mFeedAudioPlayer.play();
+    }
+
+    @Override
+    public void onProgressUpdate(Play play, float v, float v1) {
+
+    }
+
+    @Override
+    public void onPlayStarted(Play play) {
+//
+//        String str  = toJson(play.getAudioFile().getOptions());
+//
+//        try {
+//            JSONObject object = new JSONObject(str);
+//            WritableMap options  = convertJsonToMap(object);
+//            WritableMap playParams = Arguments.createMap();
+//            playParams.putMap("metadata",options);
+//            playParams.putString("id", play.getAudioFile().getId());
+//            playParams.putString("title", play.getAudioFile().getTrack().getTitle());
+//            playParams.putString("artist", play.getAudioFile().getArtist().getName());
+//            playParams.putString("album", play.getAudioFile().getRelease().getTitle());
+//            playParams.putString("artist", play.getAudioFile().getArtist().getName());
+//            playParams.putBoolean("canSkip", mFeedAudioPlayer.canSkip());
+//            playParams.putInt("duration", (int)play.getAudioFile().getDurationInSeconds());
+//            WritableMap params = Arguments.createMap();
+//            params.putMap("play", playParams);
+//            sendEvent(reactContext, "play-started", params);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    @Override
+    public void onSkipStatusChanged(boolean b) {
+
+    }
+
+    @Override
+    public void onStateChanged(FeedAudioPlayer.State state) {
+//        WritableMap params = Arguments.createMap();
+//        switch (state)
+//        {
+//            case PAUSED:                 params.putInt("state",FeedAudioPlayer.State.PAUSED.ordinal()); break;
+//            case PLAYING:                params.putInt("state",FeedAudioPlayer.State.PLAYING.ordinal()); break;
+//            case STALLED:                params.putInt("state",FeedAudioPlayer.State.STALLED.ordinal()); break;
+//            case UNAVAILABLE:            params.putInt("state",FeedAudioPlayer.State.UNAVAILABLE.ordinal()); break;
+//            case READY_TO_PLAY:          params.putInt("state",FeedAudioPlayer.State.READY_TO_PLAY.ordinal()); break;
+//            case UNINITIALIZED:          params.putInt("state",FeedAudioPlayer.State.UNINITIALIZED.ordinal()); break;
+//            case WAITING_FOR_ITEM:       params.putInt("state",FeedAudioPlayer.State.WAITING_FOR_ITEM.ordinal()); break;
+//            case AVAILABLE_OFFLINE_ONLY: params.putInt("state",FeedAudioPlayer.State.AVAILABLE_OFFLINE_ONLY.ordinal()); break;
+//        }
+//
+//        sendEvent(reactContext, "state-change", params);
+    }
+
+    @Override
+    public void onStationChanged(Station station) {
+//        callbackContext.success(message);
+//        WritableMap params = Arguments.createMap();
+//        params.putInt("activeStationId", station.getId());
+//        sendEvent(reactContext, "station-change", params);
+    }
+
+    // Skip
+    @Override
+    public void requestCompleted(boolean b) {
+//        if(!b) {
+//            WritableMap params = Arguments.createMap();
+//            sendEvent(reactContext, "skip-failed", params);
+//        }
+    }
+
+
 }
